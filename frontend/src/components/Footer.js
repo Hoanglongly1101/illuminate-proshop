a432b553f8373aaa4fbe70aa5278295ad05503c5
Hoanglongly1101 <genosider2505@gmail.com>
2021-11-15 02:39:16 +0700

    make it better

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,14 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+          <Col className='text-center py-3'>
+          Copyright &copy; Illuminate Team <br/>
+          Trần Quốc Thắng - 4501104220 <br/>
+          Lý Hoàng Long - 4501104129 <br/>
+          Đặng Phan Hoàng Sang - 4501104197 <br/>
+          Chan Hồng Vỹ - 4501104284 <br/>
+          Lê Thị Thanh Huyền - 4501104098
+          </Col>
         </Row>
       </Container>
     </footer>

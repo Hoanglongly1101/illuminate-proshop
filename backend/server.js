87e062c3b7cb14cb67ba903f42d9b46156c087dd
Hoanglongly1101 <genosider2505@gmail.com>
2021-11-15 01:36:43 +0700

    make it better

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,6 @@ import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
-
 connectDB()
 
 const app = express()
@@ -35,8 +34,9 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.use(express.static('/frontend/build'))
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
